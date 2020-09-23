adfbffef9e774383d3215e329c697c269fa6381c
Samuel <samuelalmaoherrera@gmail.com>
2020-09-23 10:21:00 -0300

    removed column from db.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,6 @@
 class User < ApplicationRecord
   validates :username, length: { minimum: 4, maximun: 12 }
-  validates :password, length: { minimum: 6, maximun: 16 }
-  validates :username, :email, :password, presence: true
+  validates :username, :email, presence: true
   validates :username, :email, uniqueness: true
   has_many :posts
   has_many :comments, through: :posts
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_203813) do
+ActiveRecord::Schema.define(version: 2020_09_23_123913) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
@@ -34,7 +34,6 @@ ActiveRecord::Schema.define(version: 2020_09_21_203813) do
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "email"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
